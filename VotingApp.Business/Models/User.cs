70811a32a450fb9dd1d9d51b97940e962e18786c
DESKTOP-EHFQK6L\Dilsan <dilyan.vlahov@gmail.com>
2022-05-16 17:07:11 +0300

    (WiP) DB set,  Authentication

---
+++ VotingApp.Business/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotingApp.Business.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string EGN { get; set; }
+        public string IndividualAuthenticationCode { get; set; }
+        public bool hasVoted { get; set; }
+
+    }
+}
---
+++ VotingApp.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VotingApp.Business.Models;
+
+namespace VotingApp.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
